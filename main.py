9e3b01c237ec816f89035aa49d9bc4e5789c43d3
Dyani Dillard <ddillard@oxy.edu>
2021-04-26 19:27:32 -0700

    randomized dots in position and color

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pyglet, time, argparse
-
+from random import randint
 import render, dot, configuration, renderqueue, scene
 
 parse = argparse.ArgumentParser()
@@ -12,8 +12,13 @@ conf = pyglet.gl.Config(sample_buffers=1, samples=4)
 window = pyglet.window.Window(width=config['window']['width'], height=config['window']['height'], config=conf)
 pyglet.gl.glClearColor(*config['window']['bgcolor'], 1)
 
+width, height = 500, 500
+window = pyglet.window.Window(width=width, height=height)
+
+
 rqueue = renderqueue.RenderQueue(window)
-scene.display_scene(window, config, "fast", rqueue, True)
+scene.display_scene(window, config, "slow", rqueue, True)
+# demo fast, medium, and slow
 
 def update(dt):
     pass
@@ -23,6 +28,13 @@ pyglet.clock.schedule_interval(update, 1/240)
 @window.event
 def on_draw():
     window.clear()
+    for i in range(10):
+        x = randint(0,width)
+        y = randint(0,height)
+        pyglet.graphics.draw(1, pyglet.gl.GL_POINTS,
+                ('v2i', (x, y)),
+                ('c3B', (255, 255, 255))
+            )
     rqueue.render()
 
 pyglet.app.run()
